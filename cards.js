02c139484a73ba1f426d0970928b7bf64858f518
johnEdmon44 <johnedmoncoc@gmail.com>
2023-02-06 22:10:02 +0800

    Add card file

---
+++ cards.js
@@ -0,0 +1,31 @@
+export const cards = [{
+    id: 1,
+    img: "",
+    clicked: false,
+    name: "Hecarim"
+},{
+    id: 2,
+    img: "",
+    clicked: false,
+    name: "Jhin"
+},{
+    id: 3,
+    img: "",
+    clicked: false,
+    name: "Akali"
+},{
+    id: 4,
+    img: "",
+    clicked: false,
+    name: "Yasuo"
+},{
+    id: 5,
+    img: "",
+    clicked: false,
+    name: "Ahri"
+},{
+    id: 6,
+    img: "",
+    clicked: false,
+    name: "Jinx"
+}, ]
