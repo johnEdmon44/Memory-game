b56d6b007b259189386a951164b0068636be1648
johnEdmon44 <johnedmoncoc@gmail.com>
2023-02-07 20:10:07 +0800

    Add header

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h2>Memory game</h2>
+        </header>
+    );
+}
