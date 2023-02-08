db8f6c7a43f8f91705c18f8cb66868efd16394f0
johnEdmon44 <johnedmoncoc@gmail.com>
2023-02-08 20:19:55 +0800

    Add score and render it

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 
 export default function Cards({ card, setCard }) {
+    const [score, setScore] = React.useState(0)
+
     const handleClick = (id) => {
         setCard(card.map(c => {
-            if (c.id === id) {
-              return { ...c, clicked: true }
+            if (c.id === id && !c.clicked) {
+                setScore(prevState => prevState + 1)
+                return { ...c, clicked: true }
+            } else {
             }
             return c
         }))
@@ -19,12 +23,15 @@ export default function Cards({ card, setCard }) {
 
 
     return (
-        <div className="container">
-            {card.map((c, index) => (
-                <div className="card" key={index} onClick={() => handleClick(c.id)}>
-                    <img src={c.img}></img>
-                </div>
-            ))}
+        <div>
+            <h1>Score: {score}</h1>
+            <div className="container">
+                {card.map((c, index) => (
+                    <div className="card" key={index} onClick={() => handleClick(c.id)}>
+                        <img src={c.img}></img>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
