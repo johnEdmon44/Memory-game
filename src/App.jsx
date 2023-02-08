f2bb4591a2d56fc28f403417aacea7aeb2a9802d
johnEdmon44 <johnedmoncoc@gmail.com>
2023-02-08 19:24:20 +0800

    Update click value to true on click

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Header />
-      <Cards card={card}/>
+      <Cards card={card} setCard={setCard} />
     </>
   );
 }
--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 
-export default function Cards({card}) {
+export default function Cards({ card, setCard }) {
+    const handleClick = (id) => {
+        setCard(card.map(c => {
+            if (c.id === id) {
+              return { ...c, clicked: true }
+            }
+            return c
+        }))
+    }
+
+
+    React.useEffect(() => {
+        console.log(card);
+    }, [card]);
+
+
+
     return (
         <div className="container">
-            {card.map((c) => (
-                <div className="card">
+            {card.map((c, index) => (
+                <div className="card" key={index} onClick={() => handleClick(c.id)}>
                     <img src={c.img}></img>
                 </div>
             ))}
